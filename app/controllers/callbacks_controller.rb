ac5ca10691f073bce6059b8553a8800561933b87
Gianpietro Lavado <gianpietro1@gmail.com>
2015-03-02 12:02:49 -0500

    corrected existing email issue with facebook

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,12 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
+        @existing_user = User.find_by(email: @user.email)
+        if @existing_user!=nil? && @existing_user.provider!=nil
+          sign_in_and_redirect @user
+        else
+          flash[:error] = "Email already exists"
+          redirect_to root_path
+        end
     end
 end
